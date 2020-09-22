e8ac1c8d3e1f5e39fa76bee2785a4086f3106d11
Karl-August Kongo <karl-august.kongo@khk.ee>
2020-09-22 09:46:12 +0300

    yl1.5 lõpplahendus

---
+++ 1/yl1.5.py
@@ -0,0 +1,7 @@
+# kasutaja sisend
+ainepunktid = int(input("Sisestage ainepunktide arv: "))
+nädalad = int(input("Sisestage nädalate arv: "))
+# arvutused
+aeg = (ainepunktid * 26) / 10
+# väljund
+print(round (aeg))
