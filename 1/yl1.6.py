f9058215877c6abe930895977c8c31a5775fd516
Karl-August Kongo <karl-august.kongo@khk.ee>
2020-09-22 10:07:46 +0300

    yl1.5 lõpplahendus

---
+++ 1/yl1.6.py
@@ -0,0 +1,12 @@
+inimesed = 146
+kohad = 32
+bussid = inimesed / kohad
+mahub = inimesed / bussid
+mahajäänud = inimesed % kohad
+print("Inimeste arv: " + str(inimesed))
+print("Kohtade arv ühes bussis: " + str(kohad))
+print("Busside arv: " + str(bussid))
+print("Bussidesse mahub: " + str(mahub) + " inimest.")
+print("Maha jäi: " + str(mahajäänud) + " inimest.")
+
+
