0138e73432b9128d5adf233ab077ebaf44d865e9
Karl-August Kongo <karl-august.kongo@khk.ee>
2020-09-22 09:34:52 +0300

    yl1.3 lõpplahendus

---
+++ 1/yl1.3.py
@@ -0,0 +1,6 @@
+# kasutaja sisend
+alus = int(input("Sisesta astme alus: "))
+astendaja = int(input("Sisesta astendaja: "))
+tulemus = alus ** astendaja # astme operaator
+# väljund
+print("Tulemus = " + str(tulemus))
