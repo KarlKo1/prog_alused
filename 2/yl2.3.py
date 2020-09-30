f8e62cb579f419f9a0bb19a6608c34135cb8390a
KarlKo1 <kongopoiss@gmail.com>
2020-09-30 20:42:22 +0300

    yl2.4py

--- 2/yl2.3.py
+++ 2/yl2.3.py
@@ -4,22 +4,22 @@ pulse_max_female = 206 - (0.88*age)
 gender = input("Sisestage enda sugu: ").upper()
 
 if gender == "M":
-	selected_gender_max_pulse = pulse_max_male
+	gender_max_pulse = pulse_max_male
 else:
-	selected_gender_max_pulse = pulse_max_female
+	gender_max_pulse = pulse_max_female
 
-training_type_id = int(input("Sisestage treeningu tüüp (1 - tervisetreening, 2 - põhivastupidavuse treening, 3 - intensiivne aeroobne treening): "))
+training_id = int(input("Sisestage treeningu tüüp (1 - tervisetreening, 2 - põhivastupidavuse treening, 3 - intensiivne aeroobne treening): "))
 
 
-if training_type_id == 1:
+if training_id == 1:
 	min_pulserate = 0.50
 	max_pulserate = 0.70
-elif training_type_id == 2:
+elif training_id == 2:
 	min_pulserate = 0.70
 	max_pulserate = 0.80
-elif training_type_id == 3:
+elif training_id == 3:
 	min_pulserate = 0.80
 	max_pulserate = 0.87
 
-print("Pulsisagedus peaks olema vahemikus " + str(round((selected_gender_max_pulse * min_pulserate))) + " kuni " + str(round((selected_gender_max_pulse * max_pulserate))) + ".")
+print("Pulsisagedus peaks olema vahemikus " + str(round((gender_max_pulse * min_pulserate))) + " kuni " + str(round((gender_max_pulse * max_pulserate))) + ".")
 
