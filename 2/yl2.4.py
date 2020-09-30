1859aa3639dfb7342c6a6062683971f900185d6d
KarlKo1 <kongopoiss@gmail.com>
2020-09-30 20:47:05 +0300

    yl2.4py

--- 2/yl2.4.py
+++ 2/yl2.4.py
@@ -1,16 +1,16 @@
 from random import randint
-user_seat = ""
-user_choice = input("Kas soovite istekohta ise valida (\"ise\") või loosida (\"loos\")?")
-if user_choice == "ise":
-    user_seat_choice = input("Kas soovite istud akna ääres (\"aken\") või mitte (\"muu\")? ")
+seat = ""
+choice = input("Kas soovite istekohta ise valida (\"ise\") või loosida (\"loos\")?")
+if choice == "ise":
+    win_or_else = input("Kas soovite istud akna ääres (\"aken\") või mitte (\"muu\")? ")
 
-    if user_seat_choice == "aken":
+    if win_or_else == "aken":
         print ("Valisite ise. Aknakoht")
     else:
-        user_seat == "muu"
+        seat == "muu"
         print("Valisite ise. Vahekäigukoht")
 
-if user_choice == "loos":
+if choice == "loos":
     seat_chance = randint(1, 3)
     if seat_chance == 1:
         print("Istekoht loositi. Aknakoht")
